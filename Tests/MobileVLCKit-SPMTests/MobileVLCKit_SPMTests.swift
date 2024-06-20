058ffa46668c632d729c0f29ee3ce734cc2fdccd
Jake.Lee <dlwjdcjfncn@gmail.com>
2024-06-20 17:06:52 +0900

    ✨ make package pjt

---
+++ Tests/MobileVLCKit-SPMTests/MobileVLCKit_SPMTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import MobileVLCKit_SPM
+
+final class MobileVLCKit_SPMTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
