9a212a50c3defc1596bff24bc0665cd2fe176385
Jake.Lee <dlwjdcjfncn@gmail.com>
2024-06-20 17:20:02 +0900

    🚀 upload for spm

--- Package.swift
+++ Package.swift
@@ -8,16 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MobileVLCKit-SPM",
-            targets: ["MobileVLCKit-SPM"]),
+            name: "MobileVLCKit",
+            targets: ["MobileVLCKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MobileVLCKit-SPM"),
-        .testTarget(
-            name: "MobileVLCKit-SPMTests",
-            dependencies: ["MobileVLCKit-SPM"]),
+      .binaryTarget(
+        name: "MobileVLCKit",
+        url: "https://github.com/PPTaa/MobileVLCKit-SPM/releases/download/3.6.0/MobileVLCKit.xcframework.zip",
+        checksum: "056c607e0b25ef0a0369c1038beec95f237d0382e81d408faec670a27d625e15"
+      )
     ]
 )
